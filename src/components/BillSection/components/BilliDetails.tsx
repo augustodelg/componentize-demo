a54baa764d18ae1424b937a435421782c53d6eaa
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-12-26 12:27:54 -0300

    BilliDetails created

---
+++ src/components/BillSection/components/BilliDetails.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react"
+import { BillItem } from "../../../entity/billi.type"
+import BilliItem from "./BilliItem"
+
+interface Props {
+    details: BillItem[]
+}
+
+export const BilliDetails = (props: Props) => {
+    return (
+        <Fragment>
+            {props.details.map((itemBill) => {
+                return (
+                    <BilliItem amount={itemBill.amount} subject={itemBill.subject} key={itemBill.subject} />
+                )
+            })}
+        </Fragment>
+    )
+}
