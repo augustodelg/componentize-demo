2104f9bf988c000e8fa7444c2c4afe00ea5b638c
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-12-26 10:48:09 -0300

    Added Product Counter

--- src/components/ItemsList/components/ItemInCart/components/InteractiveSection/index.tsx
+++ src/components/ItemsList/components/ItemInCart/components/InteractiveSection/index.tsx
@@ -1,11 +1,8 @@
 import THEME from "../../../../../../config/theme"
 import { ProductInCart } from "../../../../../../entity/product.types"
-import Button from "../../../../../generals/Button"
 import Chip from "../../../../../generals/Chip"
-import Image from "../../../../../generals/Image"
 import Typography from "../../../../../generals/Typography"
-import lessIcon from "../../../../../../assets/icons/less.svg"
-import plusIcon from "../../../../../../assets/icons/plus.svg"
+import ProductCounter from "./components/ProductCounter"
 
 interface Props extends Pick<ProductInCart, "quantity" | "price"> {
 }
@@ -15,26 +12,11 @@ const InteractiveSection = (props: Props) => {
     return (
         <div className="grid grid-row-2">
             <Chip color={THEME.colors.primary.bg} center={true}>
-                <Typography.Normal className="mx-4 p-1.5" color={THEME.colors.white.text} bold={true}  >
+                <Typography.Normal className="mx-6 p-1.5" color={THEME.colors.white.text} bold={true}  >
                     ${props.price}
                 </Typography.Normal>
             </Chip>
-
-            <Chip color={THEME.colors.secondary.bg} center={true}>
-                <div className="grid grid-cols-3">
-                    <Button.Round className="m-1" color={THEME.colors.white.bg} >
-                        <Image size="w-3/6" center={true} src={lessIcon} />
-                    </Button.Round>
-                    <Typography.Normal className="mx-auto p-1" color={THEME.colors.white.text} bold={true}>
-                        {props.quantity}
-                    </Typography.Normal>
-                    <Button.Round className="m-1" color={THEME.colors.white.bg} >
-                        <Image size="w-3/6" center={true} src={plusIcon} />
-                    </Button.Round>
-
-                </div>
-
-            </Chip>
+            <ProductCounter initialCounter={props.quantity}/>
         </div>
     )
 }
