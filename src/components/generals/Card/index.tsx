a5054c0b9e86cf19d78fae23831c07c95eed62c4
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-12-26 15:36:50 -0300

    General Card Updated

--- src/components/generals/Card/index.tsx
+++ src/components/generals/Card/index.tsx
@@ -1,10 +1,16 @@
 import { PropsWithChildren } from "react"
+import THEME from "../../../config/theme"
 
 interface Props {
-    color: string,
+    color?: string,
 }
 
+
+// Also variants of the card could be made, for example VerticalCard, HorizontalCard, etc. 
 const Card = (props: PropsWithChildren<Props>) => {
+
+    const color = props.color ? props.color : THEME.colors.white.bg
+
     return (
         <div className={`${props.color} rounded-3xl`}>
             {props.children}
