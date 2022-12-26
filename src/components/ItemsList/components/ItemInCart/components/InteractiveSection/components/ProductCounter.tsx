0ab7677d962935d89b0dea6e6394009c843a63d8
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-12-26 09:52:29 -0300

    Add ProductCounter

---
+++ src/components/ItemsList/components/ItemInCart/components/InteractiveSection/components/ProductCounter.tsx
@@ -0,0 +1,48 @@
+import THEME from "../../../../../../../config/theme"
+import Button from "../../../../../../generals/Button"
+import Chip from "../../../../../../generals/Chip"
+import Image from "../../../../../../generals/Image"
+import Typography from "../../../../../../generals/Typography"
+import lessIcon from "../../../../../../../assets/icons/less.svg"
+import plusIcon from "../../../../../../../assets/icons/plus.svg"
+import { useState } from "react"
+
+interface Props {
+    initialCounter: number
+}
+
+
+const ProductCounter = (props: Props) => {
+
+    const [cant, setCant] = useState(props.initialCounter);
+
+    const handleLess = () => {
+        if (cant > 1) {
+            setCant(cant - 1)
+        }
+    }
+
+    const handlePlus = () => {
+
+        setCant(cant + 1)
+    }
+
+    return (
+        <Chip color={THEME.colors.secondary.bg} center={true}>
+            <div className="grid grid-cols-3">
+                <Button.Round onClick={handleLess} className="m-1" color={THEME.colors.white.bg} >
+                    <Image size="w-3/6" center={true} src={lessIcon} />
+                </Button.Round>
+                <Typography.Normal className="mx-auto p-1" color={THEME.colors.white.text} bold={true}>
+                    {cant}
+                </Typography.Normal>
+                <Button.Round onClick={handlePlus} className="m-1" color={THEME.colors.white.bg} >
+                    <Image size="w-3/6" center={true} src={plusIcon} />
+                </Button.Round>
+
+            </div>
+
+        </Chip>
+    )
+}
+export default ProductCounter
