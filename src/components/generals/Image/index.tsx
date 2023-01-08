8cbb919c3838681bfa62a5aecb39ed7b4da34185
Augusto Del Grosso <adg.prodlp@gmail.com>
2023-01-08 02:16:12 +0000

    Update index.tsx

--- src/components/generals/Image/index.tsx
+++ src/components/generals/Image/index.tsx
@@ -1,19 +1,38 @@
-import { PropsWithChildren } from "react"
+import { PropsWithChildren } from "react";
 
 interface Props {
-    src: string,
-    center?: boolean,
-    size?: 'w-1/6' | 'w-2/6' | 'w-3/6' | 'w-4/6' | 'w-5/6' | 'w-6/6' | 'w-1/12' | 'w-2/12' | 'w-3/12' | 'w-4/12' | 'w-5/12' | 'w-6/12' | 'w-7/12' | 'w-8/12' | 'w-9/12' | 'w-10/12' | 'w-11/12' | 'w-12/12'
-
+  src: string;
+  center?: boolean;
+  size?:
+    | "w-1/6"
+    | "w-2/6"
+    | "w-3/6"
+    | "w-4/6"
+    | "w-5/6"
+    | "w-6/6"
+    | "w-1/12"
+    | "w-2/12"
+    | "w-3/12"
+    | "w-4/12"
+    | "w-5/12"
+    | "w-6/12"
+    | "w-7/12"
+    | "w-8/12"
+    | "w-9/12"
+    | "w-10/12"
+    | "w-11/12"
+    | "w-12/12";
 }
 
 const Image = (props: Props) => {
+  const center = props.center && "mx-auto my-auto";
 
-    const center = props.center && 'mx-auto my-auto'
+  return (
+    <img
+      src={props.src}
+      className={`max-w-full h-auto ${center} ${props.size}`}
+    />
+  );
+};
 
-    return (
-        <img src={props.src} className={`max-w-full h-auto ${center} ${props.size}`} />
-    )
-}
-
-export default Image
+export default Image;
